05ea92592c423b1f00df511b8e78ebc428b73f58
Gustavo <avoguga@gmail.com>
2022-09-09 00:41:00 -0300

    Adiciona todo login etc etc e tal

--- App.tsx
+++ App.tsx
@@ -1,6 +1,19 @@
-import Login from './src/screens/login';
+import { View, StyleSheet, useWindowDimensions } from "react-native";
+import Login from "./src/screens/login";
 export default function App() {
+  const windowHeight = useWindowDimensions().height;
+
   return (
-    <Login />
+    <View style={[styles.mainContainer, {minHeight: Math.round(windowHeight)}]}>
+      <Login />
+    </View>
   );
 }
+
+const styles = StyleSheet.create ({
+  mainContainer: {
+    flex: 1,
+    backgroundColor: "#112",
+    justifyContent: "center",
+  }
+});
--- src/screens/login/index.tsx
+++ src/screens/login/index.tsx
@@ -1,43 +1,122 @@
-import { View, Text, StyleSheet } from "react-native";
-import React from "react";
-import { Input, Button } from "@rneui/themed";
+import { View, Text, StyleSheet, ActivityIndicator } from "react-native";
+import React, { useState } from "react";
+import { Button, Input } from "@rneui/base";
+import { Formik } from "formik";
+import * as Yup from "yup";
+import { useNavigation } from "@react-navigation/native";
 
 export default function LoginScreen() {
+  const [erro, setErro] = useState<null | string>(null);
+
+  //Funções
+
+  /** Função de logar do Fã do Guga */
+  const logar = async ({ user, password }: any) => {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    if (user.trim() == "Fã do Gugas" && password == "soufan")
+      console.log("oiii");
+    else setErro("Email ou senha incorreta!");
+  };
+
   return (
-    <View style={styles.container}>
-      <View style={[styles.container, { flex: 1}]}>
-        <Text style={styles.title}>Fã Clube Do Gugas</Text>
-      </View>
-      <View style={[styles.container, { flex: 2 }]}>
-        <View style={{marginBottom: 300}}>
-          <Input
-            label="Usuário"
-            placeholder="Escreva aqui seu usuário"
-            style={{ backgroundColor: "white", width: 100 }}
-          />
-          <Input
-            label="Senha"
-            placeholder="Escreva aqui sua senha"
-            secureTextEntry
-            style={{ backgroundColor: "white", width: 100 }}
-          />
-          <Button>Login</Button>
-        </View>
-      </View>
-    </View>
+    <Formik
+      initialValues={{ user: "", password: "" }}
+      validationSchema={Yup.object({
+        user: Yup.string().required("Informe o user fazendo o favor!"),
+        password: Yup.string()
+          .required("Digite a senha")
+          .min(6, "Só aceito acima de 8 caracteres meu bom!"),
+      })}
+      onSubmit={logar}
+    >
+      {({
+        errors,
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        isSubmitting,
+        touched,
+      }) => (
+        <>
+          <Text style={styles.title}>Fã Clube Do Gugas</Text>
+          <View>
+            <View style={styles.inputContainer}>
+              <Input
+                label="Usuário"
+                placeholder="Escreva aqui seu usuário"
+                style={styles.input}
+                onChangeText={handleChange("user")}
+              />
+              {touched.user && errors.user && (
+                <Text style={styles.error}>{errors.user}</Text>
+              )}
+              <Input
+                label="Senha"
+                placeholder="Escreva aqui sua senha"
+                secureTextEntry
+                style={styles.input}
+                onChangeText={handleChange("password")}
+              />
+              {touched.password && errors.password && (
+                <Text style={styles.error}>{errors.password}</Text>
+              )}
+
+              {erro && <Text style={styles.erroLogin}>{erro}</Text>}
+
+              {isSubmitting && (
+                <ActivityIndicator size="large" color="#790808" />
+              )}
+              {!isSubmitting && (
+                <Button
+                  buttonStyle={styles.button}
+                  titleStyle={{ fontSize: 25 }}
+                  title="Login"
+                  onPress={() => handleSubmit()}
+                />
+              )}
+            </View>
+          </View>
+        </>
+      )}
+    </Formik>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#F7EBE8",
-    justifyContent: "center",
+  inputContainer: {
+    marginTop: 50,
+    marginBottom: 100,
+    alignItems: "center",
+  },
+  input: {
+    backgroundColor: "#fff",
+    width: 100,
+    borderRadius: 10,
+    padding: 10,
+    fontSize: 25,
   },
   title: {
-    fontSize: 30,
+    fontSize: 40,
     color: "#790808",
     fontWeight: "bold",
-    marginLeft: 70,
+    marginLeft: 30,
+  },
+  button: {
+    width: 250,
+    backgroundColor: "black",
+    borderRadius: 20,
+  },
+  error: {
+    color: "white",
+    fontSize: 15,
+    textAlign: "right",
+    marginBottom: 5,
+    marginTop: -10,
+  },
+  erroLogin: {
+    color: "white",
+    fontSize: 20,
+    marginBottom: 10,
+    textAlign: "center",
   },
 });
